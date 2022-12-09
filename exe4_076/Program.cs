444575cb6759d6b01a230c22829ed95ea9b2e89b
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-12-09 15:48:07 +0700

    add class node

--- exe4_076/Program.cs
+++ exe4_076/Program.cs
@@ -3,5 +3,14 @@
 namespace Exe4_076
 {
 
-
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node (int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
 }
